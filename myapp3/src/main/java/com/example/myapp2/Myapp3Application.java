129b5e375e44c72d802a47917d0823909943ac12
JINHEEWON <diebfusod1228@naver.com>
2023-12-13 11:45:35 +0900

    heewon commit

--- myapp3/src/main/java/com/example/myapp2/Myapp3Application.java
+++ myapp3/src/main/java/com/example/myapp2/Myapp3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Myapp3Application {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(Myapp3Application.class, args);
 	}
 
