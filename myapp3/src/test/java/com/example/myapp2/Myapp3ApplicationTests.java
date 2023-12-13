2b77451fd8eba468d4e23b6ae3b03ac7d9e00230
엉히킴 <cin05189@naver.com>
2023-12-13 11:44:51 +0900

    initial commit

---
+++ myapp3/src/test/java/com/example/myapp2/Myapp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myapp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
